974fdede9c697bf3bc50443f7f51d82ea3d738fd
Angelo Lapa Huamani <angelo_lapa@hotmail.com>
2020-11-22 23:47:49 -0500

    third_update_angelo

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@
               </v-list-tile-action>
               <v-list-tile-content>
                 <v-list-tile-title>
-                  Enfermeda
+                  Enfermedad
                 </v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
